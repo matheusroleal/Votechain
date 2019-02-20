0c0d34f0b8b2910fc313a0fd8b827a544f9b6719
matheus.leal <matheus.leal@corp.globo.com>
2019-02-20 20:06:56 -0300

    server config update

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"log"
   "encoding/json"
 	"net/http"
+	"time"
 
 	blockchain "github.com/matheusroleal/Votechain/blockchain"
 
@@ -27,11 +28,19 @@ func (g *CreateBlockHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func Run() {
-  addr := "0.0.0.0:8081"
-  router := httptreemux.NewContextMux()
+  // addr := "127.0.0.1:8081"
+	router := httptreemux.NewContextMux()
+	server := &http.Server{
+		Addr:         "0.0.0.0:8081",
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  time.Second,
+	}
 	blockchain.GenesisBlock()
   router.Handler(http.MethodPost, "/newTransaction/", &CreateBlockHandler{})
 
-  log.Printf("Running web server on: http://%s\n", addr)
-  log.Fatal(http.ListenAndServe(addr, router))
+  // log.Printf("Running web server on: http://%s\n", addr)
+  // log.Fatal(http.ListenAndServe(addr, router))
+	log.Fatal(server.ListenAndServe())
 }

0e3fd1108ed4211ba78cded0a4c6936bde463fe4
matheus.leal <matheus.leal@corp.globo.com>
2019-02-25 16:54:24 -0300

    discover a peer in the network using mdns

--- main.go
+++ main.go
@@ -5,23 +5,20 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
 
 	golog "github.com/ipfs/go-log"
-	peer "github.com/libp2p/go-libp2p-peer"
-	pstore "github.com/libp2p/go-libp2p-peerstore"
-	ma "github.com/multiformats/go-multiaddr"
 	gologging "github.com/whyrusleeping/go-logging"
   blockchain "github.com/matheusroleal/Votechain/blockchain"
   p2p "github.com/matheusroleal/Votechain/p2p"
+  mdns "github.com/matheusroleal/Votechain/mdns"
 )
 
 func main() {
+  blockchain.GenesisBlock()
 
 	golog.SetAllLoggers(gologging.INFO) // Change to DEBUG for extra info
 
 	listenF := flag.Int("l", 0, "wait for incoming connections")
-	target := flag.String("d", "", "target peer to dial")
 	secio := flag.Bool("secio", false, "enable secio")
 	seed := flag.Int64("seed", 0, "set random seed for id generation")
 	flag.Parse()
@@ -35,49 +32,29 @@ func main() {
 		fmt.Println(err)
 	}
 
-	if *target == "" {
-    blockchain.GenesisBlock()
+	ha.SetStreamHandler("/p2p/1.0.0", p2p.HandleStream)
 
-		fmt.Println("listening for connections")
-		ha.SetStreamHandler("/p2p/1.0.0", p2p.HandleStream)
+	peerChan := mdns.InitMDNS(context.Background(), ha, "meetmehere")
 
-		select {}
+	peer := <-peerChan // will block untill we discover a peer
+	fmt.Println("Found peer:", peer, ", connecting")
 
-	} else {
-		ha.SetStreamHandler("/p2p/1.0.0", p2p.HandleStream)
-
-		ipfsaddr, err := ma.NewMultiaddr(*target)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		pid, err := ipfsaddr.ValueForProtocol(ma.P_IPFS)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		peerid, err := peer.IDB58Decode(pid)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		targetPeerAddr, _ := ma.NewMultiaddr(
-			fmt.Sprintf("/ipfs/%s", peer.IDB58Encode(peerid)))
-		targetAddr := ipfsaddr.Decapsulate(targetPeerAddr)
-
-		ha.Peerstore().AddAddr(peerid, targetAddr, pstore.PermanentAddrTTL)
-
-		log.Println("opening stream")
-		s, err := ha.NewStream(context.Background(), peerid, "/p2p/1.0.0")
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
-
-		go p2p.WriteData(rw)
-		go p2p.ReadData(rw)
-
-		select {}
+	err = ha.Connect(context.Background(), peer)
+  if err != nil {
+		fmt.Println("Connection failed:", err)
 	}
+
+	// open a stream, this stream will be handled by handleStream other end
+	stream, err := ha.NewStream(context.Background(),peer.ID, "/p2p/1.0.0")
+
+	if err != nil {
+		fmt.Println("Stream open failed", err)
+	} else {
+		rw := bufio.NewReadWriter(bufio.NewReader(stream), bufio.NewWriter(stream))
+
+  	go p2p.WriteData(rw)
+		go p2p.ReadData(rw)
+	}
+
+	select {}
 }
